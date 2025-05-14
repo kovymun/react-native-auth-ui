90f5a3b8e912e1d277794d97b498020dcd3defe2
Kovy <codingstudent2021@gmail.com>
2025-05-14 22:30:11 +0200

    Implemented login UI with email and password inputs

---
+++ app/register.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const register = () => {
+  return (
+    <Text style={{ fontFamily: "Poppins_600SemiBold", fontSize: 24 }}>
+      Register
+    </Text>
+  );
+};
+
+export default register;
+
+const styles = StyleSheet.create({});
